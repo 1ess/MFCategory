21037195d1817cce2716d53774b9f289d17459c8
GodzzZZZ <pipeliningzzz@gmail.com>
2018-02-08 15:18:48 +0800

    新增 SaveImageManager

---
+++ Classes/Manager/MFSavePhotoManager.m
@@ -0,0 +1,44 @@
+//
+//  MFSavePhotoManager.m
+//  软装
+//
+//  Created by 张冬冬 on 2018/2/8.
+//  Copyright © 2018年 张冬冬. All rights reserved.
+//
+
+#import "MFSavePhotoManager.h"
+#import <Photos/Photos.h>
+@implementation MFSavePhotoManager
++ (void)saveImage:(UIImage *)image
+          success:(successHandler)successHandle
+          failure:(failureHandler)failureHanlde {
+    dispatch_async(dispatch_get_main_queue(), ^{
+        [[PHPhotoLibrary sharedPhotoLibrary] performChanges:^{
+            [PHAssetChangeRequest creationRequestForAssetFromImage:image];
+        } completionHandler:^(BOOL success, NSError * _Nullable error) {
+            if (success && successHandle) {
+                successHandle();
+            }else if (error && failureHanlde) {
+                failureHanlde(error);
+            }
+        }];
+    });
+}
+
++ (void)saveImageData:(NSData *)imageData
+              success:(successHandler)successHandle
+              failure:(failureHandler)failureHanlde {
+    dispatch_async(dispatch_get_main_queue(), ^{
+        [[PHPhotoLibrary sharedPhotoLibrary] performChanges:^{
+            PHAssetResourceCreationOptions *options = [[PHAssetResourceCreationOptions alloc] init];
+            [[PHAssetCreationRequest creationRequestForAsset] addResourceWithType:PHAssetResourceTypePhoto data:imageData options:options];
+        } completionHandler:^(BOOL success, NSError * _Nullable error) {
+            if (success && successHandle) {
+                successHandle();
+            }else if (error && failureHanlde) {
+                failureHanlde(error);      
+            }
+        }];
+    });
+}
+@end
